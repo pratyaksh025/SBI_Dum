2db7c4afc8c979cf8b033bd08e7a852724b6a96b
Pratyaksh Yadav <ypratyaksh59@gmail.com>
2025-03-03 12:41:37 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,10 +2,10 @@ import mysql.connector as msc
 
 def mycon():
     conn= msc.connect(
-    host="mydb.cbawyqc62v0z.us-east-1.rds.amazonaws.com",
-    username="prat",
-    password="Pass123456789",
-    database="sbidb"
+    host="sql12.freesqldatabase.com",
+    username="sql12765630",
+    password="etEtC47gwJ",
+    database="sql12765630"
     )
     return conn
 
